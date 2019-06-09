34c9cf559e25d64ee368d3426a71171c142d75f3
Ryan Foster <rfosterr91@gmail.com>
2019-06-09 13:21:40 -0700

    fleshed out the javascript

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,12 +6,23 @@ var control=7;
 var letID = " ";
 var guess = "a";
 var gameCheck = 0;
-var rand = 1;
+var rand = Math.floor(Math.random() * 3);;
 var winCount = 0;
+var picSwap;
 
 var marioGame = {
     letterAmount : 5,
     name : ["m","a","r","i","o"],
+}
+
+var linkGame = {
+    letterAmount : 4,
+    name : ["l","i","n","k"],
+}
+
+var foxGame = {
+    letterAmount : 3,
+    name : ["f","o","x"],
 }
 
 function boardMaker(control) {  
@@ -30,50 +41,164 @@ function boardMaker(control) {
 function letterChanger(letID, guess){
     document.getElementById(letID).innerHTML = guess;
 }
+function letterChecker(letID){
+    if(document.getElementById(letID).innerHTML=== guess)
+    {
+        winCount--;
+    }
+}
 if (rand === 1){
     boardMaker(marioGame.letterAmount);
-        for (index=0;index<99;index++){
-            guess = prompt("Enter a letter");
+        document.onkeyup = function(event){
+            guess = event.key;
             if (guess === "m"){
+                letterChecker("1");
+                letterChanger("1",guess);
                 winCount++
-                letterChanger("1",guess)
             }
             else if(guess === "a"){
+                letterChecker("2");
+                letterChanger("2",guess);
                 winCount++
-                letterChanger("2",guess)
             }
             else if(guess === "r"){
+                letterChecker("3");
+                letterChanger("3",guess);
                 winCount++
-                letterChanger("3",guess)
             }
             else if(guess === "i"){
+                letterChecker("4");
+                letterChanger("4",guess);
                 winCount++
-                letterChanger("4",guess)
             }
             else if(guess === "o"){
+                letterChecker("5");
+                letterChanger("5",guess);
                 winCount++
-                letterChanger("5",guess)
             }
             else{
                 if(gameCheck === 0){
-                gameCheck++;
-                document.getElementById("stock1").style.opacity = 0;
+                    gameCheck++;
+                    document.getElementById("stock1").style.opacity = 0;
                 }
                 else if(gameCheck === 1){
                     gameCheck++;
                     document.getElementById("stock2").style.opacity = 0;
                 }
                 else{
-                    index = 100;
                     document.getElementById("stock3").style.opacity = 0;
-
+                    document.onkeyup = function(event){
+                        alert("YOU LOSE! Refresh To Retry")
+                    }
                 }
             }
             if (winCount === marioGame.name.length){
-                index = 99;
-                alert("You Win");
+                alert("YOU WIN!!! Refresh To Play Again")
+                document.onkeyup = function(event){
+                    alert("YOU WIN!!! Refresh To Play Again")
+                }
             }
-        }         
+        } 
+            
+}
+
+if (rand === 0){
+    boardMaker(linkGame.letterAmount);
+        document.onkeyup = function(event){
+            guess = event.key;
+            if (guess === "l"){
+                letterChecker("1");
+                letterChanger("1",guess)
+                winCount++
+            }
+            else if(guess === "i"){
+                letterChecker("2");
+                letterChanger("2",guess)
+                winCount++
+            }
+            else if(guess === "n"){
+                letterChecker("3");
+                letterChanger("3",guess)
+                winCount++
+            }
+            else if(guess === "k"){
+                letterChecker("4");
+                letterChanger("4",guess)
+                winCount++
+            }
+            else{
+                if(gameCheck === 0){
+                    gameCheck++;
+                    document.getElementById("stock1").style.opacity = 0;
+                }
+                else if(gameCheck === 1){
+                    gameCheck++;
+                    document.getElementById("stock2").style.opacity = 0;
+                }
+                else{
+                    document.getElementById("stock3").style.opacity = 0;
+                    document.onkeyup = function(event){
+                        alert("YOU LOSE! Refresh To Retry")
+                    }
+                }
+            }
+            if (winCount === linkGame.name.length){
+ 
+                alert("YOU WIN!!! Refresh To Play Again")
+                document.onkeyup = function(event){
+                    alert("YOU WIN!!! Refresh To Play Again")
+                }
+
+            }
+        } 
+            
+}
+
+if (rand === 2){
+    boardMaker(foxGame.letterAmount);
+        document.onkeyup = function(event){
+            guess = event.key;
+            if (guess === "f"){
+                letterChecker("1");
+                letterChanger("1",guess)
+                winCount++
+            }
+            else if(guess === "o"){
+                letterChecker("2");
+                letterChanger("2",guess)
+                winCount++
+            }
+            else if(guess === "x"){
+                letterChecker("3");
+                letterChanger("3",guess)
+                winCount++
+            }
+            else{
+                if(gameCheck === 0){
+                    gameCheck++;
+                    document.getElementById("stock1").style.opacity = 0;
+                }
+                else if(gameCheck === 1){
+                    gameCheck++;
+                    document.getElementById("stock2").style.opacity = 0;
+                }
+                else{
+                    document.getElementById("stock3").style.opacity = 0;
+                    document.onkeyup = function(event){
+                        alert("YOU LOSE! Refresh To Retry")
+                    }
+                }
+            }
+            if (winCount === foxGame.name.length){
+                alert("YOU WIN!!! Refresh To Play Again")
+                document.onkeyup = function(event){
+                    alert("YOU WIN!!! Refresh To Play Again")
+                }
+
+                
+            }
+        } 
+            
 }
 
 
